1286a5d13ca2c637ccef4f2d2aa450f54d73854d
Eric Fritz <eric@eric-fritz.com>
2012-11-19 10:08:44 -0600

    Add jump point search (unimplemented) to driver.

--- src/edu/uwm/ai/search/Search.java
+++ src/edu/uwm/ai/search/Search.java
@@ -31,6 +31,7 @@ import edu.uwm.ai.search.heuristic.ManhattanDistance;
 import edu.uwm.ai.search.search.AStarSearch;
 import edu.uwm.ai.search.search.BreadthFirstSearch;
 import edu.uwm.ai.search.search.IterativeDeepening;
+import edu.uwm.ai.search.search.JumpPointSearch;
 
 import processing.core.PApplet;
 
@@ -59,6 +60,7 @@ public class Search extends PApplet
 		entities.add(new SearchEntity(this, world, world.getRandomFreePoint(), color(0, 127, 255), player, new BreadthFirstSearch(world)));
 		entities.add(new SearchEntity(this, world, world.getRandomFreePoint(), color(255, 0, 127), player, new IterativeDeepening(world)));
 		entities.add(new SearchEntity(this, world, world.getRandomFreePoint(), color(127, 0, 255), player, new AStarSearch(world, new ManhattanDistance())));
+		entities.add(new SearchEntity(this, world, world.getRandomFreePoint(), color(255, 127, 0), player, new JumpPointSearch(world)));
 
 		size(displayWidth, displayHeight + (world.getBlockHeight() * entities.size()));
 	}
